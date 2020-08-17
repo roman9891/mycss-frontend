d1870fe16a6710b4524f419240d61f3eb3ab4cfa
roman9891 <roman9891@gmail.com>
2020-08-17 10:18:27 -0400

    copied some selectors into html and added custom

---
+++ src/index.js
@@ -0,0 +1,10 @@
+document.addEventListener(`DOMContentLoaded`, e => {
+    
+    const customDiv = document.querySelector(`#custom`)
+
+    document.addEventListener(`change`, e => {
+        console.log(e.target.value)
+    })
+    
+    
+})
