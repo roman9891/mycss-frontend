37fd89ff91177d78ee0be84d7642003eff11cae8
roman9891 <roman9891@gmail.com>
2020-08-17 16:25:21 -0400

    made borderstyle and color listeners

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
 document.addEventListener(`DOMContentLoaded`, e => {
-    
+    const styleTag = document.querySelector(`#style`)
     const customDiv = document.querySelector(`#custom`)
+    const styleObject = {
+        borderStyle: ``,
+        borderColor: ``
+    }
+    const convertStyle = () => {
+        
+    }
+    const renderStyle = () => {
+        
+    }
 
     document.addEventListener(`change`, e => {
-        console.log(e.target.value)
+        
+        if (e.target.matches(`#border-style`)) {
+            console.log(e.target.value, styleTag)
+            // customDiv.style.borderStyle = e.target.value
+        }
+
+        if (e.target.matches(`#border-color`)) {
+            console.log(e.target.value, styleTag)
+            // customDiv.style.borderColor = e.target.value
+        }
+    })
+
+    document.addEventListener(`submit`, e => {
+        e.preventDefault()
+        console.log(e.target.color.value)
+        if (e.target.matches(`#border-color`)) {
+            console.log(e.target.color.value, styleTag)
+            // styleTag.innerHTML = 
+            // `
+            //     #custom {
+            //         border: 1px solid ${e.target.color.value};
+            //     }
+            // `
+        }
+        //customDiv.style.borderColor = e.target.color.value
     })
     
     
